4696f17ea95e0def2b8106dc2cf6bec10c609b77
aidai beishekeeva <aidaibeishekeeva@aidais-MacBook-Pro.local>
2021-10-31 12:16:28 -0400

    landing & questions page. draft

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,9 @@
+class UsersController < ApplicationController
+
+    # def index 
+      
+    # end 
+    def questions
+      @questions = ['Where are you moving from?', 'Where are you moving to?']
+    end 
+end
