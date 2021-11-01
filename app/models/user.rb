efd187897e1e7a9aaa068ce7c4fa3c4d3bfd8d2c
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-11-01 11:54:39 -0400

    new user model version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  has_secure_password
   enum work: {student: 0, professional: 1}
 end
--- db/migrate/20211101154711_create_users.rb
+++ db/migrate/20211101154711_create_users.rb
@@ -2,6 +2,8 @@ class CreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       t.string :username, limit: 32
+      t.string :email
+      t.string :password_digest
       t.string :from_country
       t.string :to_city
       t.integer :work
