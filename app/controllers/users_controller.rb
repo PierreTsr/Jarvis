f8c32e330d05d665801779fd02e432de36d99fda
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-11-02 19:23:19 -0400

    add users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
 
-    # def index 
-      
-    # end 
+    # def index
+    # end
+
+    def create
+      answers = params[:answers]
+      unless User.check_answers?(answers)
+        redirect_to questions_path
+        flash.alert = "Sorry, we are unable to parse your answers. Please try again."
+      else
+        User.create(answers)
+        redirect_to dashboard_path
+      end
+    end
+
     def questions
-      @questions = ['Where are you moving from?', 'Where are you moving to?', 'What\'s your budget?']
+      @questions = User.get_questions
     end 
 end
