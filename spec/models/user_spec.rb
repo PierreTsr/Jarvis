7b433a814ae4ec66c076f9d030767236764958a6
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-11-02 17:43:06 -0400

    add rspec for User and UsersController

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ":get_questions" do
+
+    before :each do
+      @questions = User.get_questions
+    end
+
+    it "is a Hash(Id,String)" do
+      @questions.is_a? Hash
+      @questions.each do |id, question|
+        expect(id).to be_a Symbol
+        expect(question).to respond_to(:to_s)
+      end
+    end
+
+    it "matches the model attributes" do
+      const_fields = [:id, :username, :email, :password_digest, :created_at, :updated_at]
+      User.column_names do |id|
+        expect(@question.keys).to include id unless const_fields.include? id
+      end
+    end
+  end
+
+  describe ":check_answers" do
+
+    before :each do
+      @answers = {
+        from_country: "France",
+        to_city: "New York",
+        work: 0,
+        budget: 1
+      }
+    end
+
+    it "accepts correct answers" do
+      expect(User.check_answers @answers).to be_truthy
+    end
+
+    User.get_questions.each_key do |key|
+      it "rejects missing answer: #{key}" do
+        @answers.delete key
+        expect(User.check_answers @answers).to be_falsey
+      end
+    end
+
+    User.get_questions.each_key do |key|
+      it "rejects wrong type: #{key}" do
+        @answers[key] = 0
+        int = User.check_answers @answers
+        @answers[key] = "abc"
+        str = User.check_answers @answers
+        expect(str && int).to be_falsey
+      end
+    end
+  end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,4 +4,56 @@ RSpec.describe "Users", type: :request do
   describe "GET /index" do
     pending "add some examples (or delete) #{__FILE__}"
   end
+
+  describe "GET /questions" do
+
+    it 'asks questions to the users' do
+      expect(User).to receive(:get_questions).and_return({
+        from_country: "Which country are you coming from?",
+        to_city: "Which city are you going to?",
+        budget: "What is your budget?"
+      })
+      get questions_path
+      expect(response).to render_template(questions_path)
+    end
+  end
+
+  describe "POST /create" do
+
+    it "creates a new user if answers are OK" do
+      answers = {
+        from_country: "USA",
+        to_city: "NYC",
+        budget: 3
+      }
+      expect(User).to receive(:check_answers)
+      expect(User).to receive(:create)
+      post user_path, params: { answers: answers }
+      expect(response).to redirect_to(dashboard_path)
+    end
+    describe "doesn't create a new user when answers" do
+
+      it "are empty" do
+        answers = {
+          from_country: "",
+          to_city: "NYC",
+          budget: 3
+        }
+        expect(User).to receive(:check_answers)
+        post user_path, params: { answers: answers }
+        expect(response).to redirect_to(questions_path)
+      end
+
+      it "are incorrect" do
+        answers = {
+          from_country: "USA",
+          to_city: "NYC",
+          budget: "a"
+        }
+        expect(User).to receive(:check_answers)
+        post user_path, params: { answers: answers }
+        expect(response).to redirect_to(questions_path)
+      end
+    end
+  end
 end
