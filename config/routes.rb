30ffaee7bb09f6c4adae56a7ef13cc5d4c6f6693
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-11-02 14:51:09 -0400

    add form and users routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
-  get '/questions', to: 'users#questions', as: 'questions'
+  get 'user/questions', to: 'users#questions', as: 'questions'
+  resource :user
+
 end
