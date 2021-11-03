b3233ada474746e450e4bcb42afcb8fff68ffc69
UnicornoftheSea99 <erhyu@smith.edu>
2021-11-03 01:24:03 -0400

    created recommendations page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
   get '/questions', to: 'users#questions', as: 'questions'
+  get '/recommendations', to: 'users#recommendations', as: 'recommendations'
 end
