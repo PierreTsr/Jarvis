3f3bf0c60144fc5c5b02d3cd7fc39a35686c23c6
Pierre Tessier <pierre.borie.tessier@gmail.com>
2021-11-14 17:40:49 -0500

    initial devise integration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_01_154711) do
+ActiveRecord::Schema.define(version: 2021_11_11_154409) do
 
   create_table "businesses", force: :cascade do |t|
     t.string "name"
@@ -30,15 +30,19 @@ ActiveRecord::Schema.define(version: 2021_11_01_154711) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "email"
-    t.string "password_digest"
     t.string "from_country"
     t.string "to_city"
     t.integer "work"
     t.integer "budget"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["username"], name: "index_users_on_username", unique: true
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
